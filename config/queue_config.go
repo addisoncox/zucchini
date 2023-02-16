9d09603e3bfd73b34ae788691842181087e5fdbf
Addison Cox <addisoncox@protonmail.com>
2023-02-15 19:46:20 -0500

    start handle retry

--- config/queue_config.go
+++ config/queue_config.go
@@ -1,10 +1,26 @@
 package config
 
-import "github.com/addisoncox/zucchini/redis"
+import (
+	"time"
+
+	"github.com/addisoncox/zucchini/redis"
+)
+
+type RetryStrategy uint
+
+const (
+	ExponentialBackoff RetryStrategy = iota
+	ExponentialBackoffWithJitter
+	Custom
+)
 
 type QueueConfig struct {
 	Name           string
 	Capacity       uint64
 	Redis          redis.RedisClient
 	GoroutineLimit uint64
+	RetryStrategy  RetryStrategy
+	RetryLimit     uint
+	// given nth retry, return how much time to wait
+	CustomRetryFunction func(uint) time.Duration
 }
--- queue/queue.go
+++ queue/queue.go
@@ -9,20 +9,32 @@ import (
 	"github.com/addisoncox/zucchini/util"
 )
 
+type Task struct {
+	task task.Task
+	id   uint64
+}
+
 type Queue struct {
 	name              string
-	tasks             chan task.Task
+	tasks             chan Task
 	redis             *redis.RedisClient
 	capacity          uint64
 	taskCount         uint64
 	goroutinesRunning uint64
 	goroutineLimit    uint64
 	callback          func(task.TaskResult)
+	taskIDCounter     uint64
+	taskRetryCounter  map[uint64]uint
+	retryLimit        uint
 }
 
 func (q *Queue) EnqueueTask(task task.Task) {
 	if q.taskCount < q.capacity {
-		q.tasks <- task
+		q.taskIDCounter++
+		q.tasks <- Task{
+			task: task,
+			id:   q.taskIDCounter,
+		}
 		util.AtomicInc(&q.taskCount)
 	} else {
 		panic("Tried to enqueue more tasks than queue capacity")
@@ -32,13 +44,25 @@ func (q *Queue) EnqueueTask(task task.Task) {
 func (q *Queue) RunNextTask() {
 	if q.taskCount > 0 {
 		nextTask := <-q.tasks
-		taskResult := util.Call(nextTask.Function, nextTask.Arguments...)
-		util.AtomicDec(&q.taskCount)
-		q.redis.LPush(q.name, taskResult)
+		q.processTask(
+			nextTask.task.Function,
+			nextTask.task.Timeout,
+			nextTask.id,
+			nextTask.task.Arguments...,
+		)
 	}
 }
 
-func (q *Queue) processTask(function interface{}, timeout time.Duration, arguments ...interface{}) {
+func (q *Queue) handleTimeout() {
+	// TODO
+}
+
+func (q *Queue) processTask(
+	function interface{},
+	timeout time.Duration,
+	taskID uint64,
+	arguments ...interface{},
+) {
 	result := make(chan task.TaskResult, 1)
 	go func() {
 		result <- util.Call(function, arguments...)
@@ -49,12 +73,23 @@ func (q *Queue) processTask(function interface{}, timeout time.Duration, argumen
 		q.redis.LPush(q.name, taskResult)
 		util.AtomicDec(&q.goroutinesRunning)
 	case <-time.After(timeout):
-		taskResult := task.TaskResult{
-			Status: task.Timeout,
-			Value:  "",
+		var taskResult task.TaskResult
+		if q.taskRetryCounter[taskID] > q.retryLimit {
+			taskResult = task.TaskResult{
+				Status: task.Failed,
+				Value:  "",
+			}
+			q.taskRetryCounter[taskID] = 0
+			util.AtomicDec(&q.goroutinesRunning)
+		} else {
+			taskResult = task.TaskResult{
+				Status: task.Timeout,
+				Value:  "",
+			}
+			q.handleTimeout()
+			q.processTask(function, timeout, taskID, arguments...)
 		}
 		q.redis.LPush(q.name, taskResult)
-		util.AtomicDec(&q.goroutinesRunning)
 	}
 }
 
@@ -73,7 +108,12 @@ func (q *Queue) ProcessTasks() {
 				nextTask := <-q.tasks
 				q.taskCount--
 				q.goroutinesRunning++
-				go q.processTask(nextTask.Function, nextTask.Timeout, nextTask.Arguments...)
+				go q.processTask(
+					nextTask.task.Function,
+					nextTask.task.Timeout,
+					nextTask.id,
+					nextTask.task.Arguments...,
+				)
 			} else {
 				break
 			}
@@ -109,12 +149,15 @@ func (q *Queue) Listen() {
 func NewQueue(cfg config.QueueConfig) Queue {
 	return Queue{
 		name:              cfg.Name,
-		tasks:             make(chan task.Task, cfg.Capacity),
+		tasks:             make(chan Task, cfg.Capacity),
 		redis:             &cfg.Redis,
 		capacity:          cfg.Capacity,
 		taskCount:         0,
 		goroutinesRunning: 0,
 		goroutineLimit:    cfg.GoroutineLimit,
 		callback:          nil,
+		taskIDCounter:     0,
+		taskRetryCounter:  make(map[uint64]uint),
+		retryLimit:        cfg.RetryLimit,
 	}
 }
