216f84d20b45c79609b3cfc43e79486bc5cce6e2
Addison Cox <addisoncox@protonmail.com>
2023-02-21 16:23:52 -0500

    api refactor II

--- example/sleep_add.go
+++ example/sleep_add.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"time"
 
-	"github.com/addisoncox/zucchini/config"
-	"github.com/addisoncox/zucchini/queue"
+	"github.com/addisoncox/zucchini/consumer"
+	"github.com/addisoncox/zucchini/producer"
 	"github.com/addisoncox/zucchini/redis"
 	"github.com/addisoncox/zucchini/task"
 )
@@ -16,40 +15,39 @@ type Numbers struct {
 	Y int
 }
 
-func sleepAdd(data []byte) interface{} {
-	var numbers Numbers
-	json.Unmarshal(data, &numbers)
+func sleepAdd(numbers Numbers) int {
 	time.Sleep(time.Second * 3)
 	return numbers.X + numbers.Y
 }
 
+func sleepAddCallback(status task.TaskStatus, res int) error {
+	fmt.Println("CALLBACK RUNNING")
+	fmt.Println(res)
+	return nil
+}
+
 func main() {
-	testTask := task.Task{
-		Name:    "test",
-		Data:    Numbers{X: 3, Y: 4},
-		Timeout: time.Second * 5,
-	}
-	queueConfig := config.QueueConfig{
-		Name:           "TestQueue",
-		Capacity:       10,
-		Redis:          *redis.NewClient("localhost:6379", "", 0),
-		GoroutineLimit: 32,
-	}
-	testQueue := queue.NewQueue(queueConfig)
-
-	processQueueConfig := config.QueueConfig{
-		Name:           "TestQueue",
-		Capacity:       10,
-		Redis:          *redis.NewClient("localhost:6379", "", 0),
-		GoroutineLimit: 32,
+	sleepAddTaskDefinition := task.TaskDefinition[Numbers, int]{
+		TaskHandler:   sleepAdd,
+		TaskCallback:  sleepAddCallback,
+		Timeout:       time.Second * 5,
+		TaskName:      "sleepAdd",
+		MaxRetries:    2,
+		RetryStrategy: task.ExponentialBackoff,
 	}
 
-	processQueue := queue.NewQueue(processQueueConfig)
-	testQueue.RegisterCallback(func(res task.TaskResult) {
-		fmt.Println("CALLBACK RUNNING")
-		fmt.Println(res.Value)
-	})
-	testQueue.EnqueueTask(testTask)
-	go processQueue.ProcessTasks(sleepAdd)
-	testQueue.Listen()
+	taskProducer := producer.NewProducer(
+		sleepAddTaskDefinition,
+		redis.NewClient("localhost:6379", "", 0),
+		10,
+	)
+	taskConsumer := consumer.NewConsumer(
+		sleepAddTaskDefinition,
+		redis.NewClient("localhost:6379", "", 0),
+		10,
+	)
+
+	taskProducer.QueueTask(Numbers{X: 3, Y: 4})
+	go taskConsumer.ProcessTasks()
+	taskProducer.AwaitCallback()
 }
